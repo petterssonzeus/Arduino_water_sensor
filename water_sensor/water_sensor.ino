99124ebeab8e698b2244095c9b9efb3786e6a2e3
petterssonzeus <gustavp21@gmail.com>
2026-03-25 21:31:24 +0100

    First successful Arduino test :)

--- water_sensor/water_sensor.ino
+++ water_sensor/water_sensor.ino
@@ -1,6 +1,17 @@
+int waterlevel;
 void setup() {
-
+  pinMode(A0, INPUT); //pinmode är en funktion (inom parentes är parametrarna, alltså pin och funktion)
+  pinMode(12, OUTPUT);
+  Serial.begin(9600); 
 }
-//Updating main
+
 void loop() {
+  waterlevel = analogRead(A0);
+  Serial.println(waterlevel);
+  if (waterlevel > 600) {
+    digitalWrite(12, HIGH);
+  }
+  else {
+    digitalWrite(12, LOW);
+  }
 }
